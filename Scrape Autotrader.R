f36e61fbb510d1bd12fbdd1659a409c80a85805a
Jake Nichols <nichols.jacob@gmail.com>
2019-11-01 20:25:35 -0400

    Update with all drive types

--- Scrape Autotrader.R
+++ Scrape Autotrader.R
@@ -5,18 +5,19 @@ library(rvest)
 #
 
 RADIUS <- 100
-MIN_PRICE = 14000
-MAX_PRICE = 28000
+MIN_PRICE = 7000
+MAX_PRICE = 40000
 PULL_RECORDS = 100
-FROM_X <- c(0,100,200,300,400,500,600,700,800,900,1000)
+FROM_X <- c(0,100,200,300,400,500,600,
+            700,800,900,1000)
 
-pages_list <- vector("list", 10)
+pages_list <- vector("list", length(FROM_X))
 
-for(i in 1:10) {
+for(i in 1:length(FROM_X)) {
   
   FROM <- FROM_X[[i]]
   
-  URL <- paste0('https://www.autotrader.com/rest/searchresults/base?vehicleStyleCodes=SEDAN&driveGroup=AWD4WD&listingTypes=USED&searchRadius=',
+  URL <- paste0('https://www.autotrader.com/rest/searchresults/base?vehicleStyleCodes=SEDAN&listingTypes=USED&searchRadius=',
                 RADIUS, 
                 '&zip=18501&minPrice=', 
                 MIN_PRICE, 
@@ -73,21 +74,89 @@ for(i in 1:10) {
       
     }
     
+    if(is.null(LISTINGS[[v]]$specifications$mileage$value)) {
+      
+      Mileage = NA
+      
+    } else {
+      
+      Mileage = LISTINGS[[v]]$specifications$mileage$value
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$model)) {
+      
+      Model = "Missing"
+      
+    } else {
+      
+      Model = LISTINGS[[v]]$model
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$ownerName)) {
+      
+      Dealer = "Missing"
+      
+    } else {
+      
+      Dealer = LISTINGS[[v]]$ownerName
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$pricingDetail$primary)) {
+      
+      Price = NA
+      
+    } else {
+      
+      Price = LISTINGS[[v]]$pricingDetail$primary
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$specifications$engine$value)) {
+      
+      Engine = "Missing"
+      
+    } else {
+      
+      Engine = LISTINGS[[v]]$specifications$engine$value
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$specifications$driveType$value)) {
+      
+      DriveType = "Missing"
+      
+    } else {
+      
+      DriveType = LISTINGS[[v]]$specifications$driveType$value
+      
+    }
+    
+    if(is.null(LISTINGS[[v]]$year)) {
+      
+      Year = "Missing"
+      
+    } else {
+      
+      Year = LISTINGS[[v]]$year
+      
+    }
+    
     df <- tibble(
       
       Make = Make,
-      Model = LISTINGS[[v]]$model,
-      Dealer = LISTINGS[[v]]$ownerName,
+      Model = Model,
+      Dealer = Dealer,
       #Phone = LISTINGS[[v]]$phone$value,
-      Price = LISTINGS[[v]]$pricingDetail$primary,
+      Price = Price,
       Color = Color,
       #MPG = LISTINGS[[v]]$specifications$mpg$value,
-      Engine = LISTINGS[[v]]$specifications$engine$value,
-      DriveType = LISTINGS[[v]]$specifications$driveType$value,
-      Mileage = LISTINGS[[v]]$specifications$mileage$value,
-      Year = LISTINGS[[v]]$year,
-      Style = LISTINGS[[v]]$style[[1]],
-      ZipCode = LISTINGS[[v]]$zip,
+      Engine = Engine,
+      DriveType = DriveType,
+      Mileage = Mileage,
+      Year = Year,
       Features = features %>%
         .[[1]],
       DealIndicator = DealIndicator
@@ -112,25 +181,34 @@ listings_data <- bind_rows(pages_list) %>%
 
 library(lme4)
 
-mod <- lmer(Price ~ (1 | Make) + Mileage + YearVar, data = listings_data)
+mod <- lmer(Price ~ (1 | Car) + (1 | Make) + Mileage + YearVar + (DealIndicator), 
+            
+            data = listings_data %>%
+              mutate(Car = paste0(Make, Model)))
 
-# everything 1000 miles drops price by $55
-# each year old drops price by $600
-# GREAT deal = $1000 off
-# None deal = $600 more
+# everything 1000 miles drops price by $75
+# each year old drops price by $1350
+# GREAT deal = $1400 off
+# None deal = $1000 more
 # Make impact
-# Mazda, Ford, Subaru are $4000 off
-# Genesis, Lexus, Benz are $2000+ more
+# Benz, Cadillac, Audi, BMW (5k)
+# Genesis, Lincoln, Volvo (3k+)
 
-makes <- ranef(mod)[[1]] %>% rownames_to_column()
+# Hyundai, Volks, Kia, Chevy, Nissan (-4k)
+# Toyota, Honda, Ford, Mazda (-2k)
+
+models <- ranef(mod)[[1]] %>% rownames_to_column()
+makes <- ranef(mod)[[2]] %>% rownames_to_column()
 
 #
 
 preds <- cbind(
   
-  pred = predict(mod, listings_data),
+  pred = predict(mod, listings_data %>%
+                   mutate(Car = paste0(Make, Model))),
   
-  listings_data
+  listings_data %>%
+    mutate(Car = paste0(Make, Model))
   
 ) %>%
   
